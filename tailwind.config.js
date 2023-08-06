27344a86f0dfc1f1f9f14da017f47875d6550036
github ryotaroinagaki <aaadddryo@gmail.com>
2023-08-06 21:46:31 +0900

    ✨ daisyUI導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    [require("daisyui")],
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
