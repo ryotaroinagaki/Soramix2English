e99f26546a3ecabf2bf39bd4ee2622bebc1e46ff
github ryotaroinagaki <aaadddryo@gmail.com>
2023-08-07 12:56:07 +0900

    トップページを作成

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,3 @@
+class TopController < ApplicationController
+  def top; end
+end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module Soramix2English
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
