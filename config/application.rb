eb8984afa95571824fd0064ddacda43323847a88
github ryotaroinagaki <aaadddryo@gmail.com>
2023-08-09 08:14:20 +0900

    Add: 国際化i18nを追加

--- config/application.rb
+++ config/application.rb
@@ -38,8 +38,13 @@ module Soramix2English
       g.skip_routes true
       g.assets false
       g.helper false
-      g.test_framework :rspec, view_specs: false, helper_specs: false, routing_specs: false,
-                               controller_specs: false
+      g.test_framework :rspec, view_specs: false, helper_specs: false, routing_specs: false,controller_specs: false
     end
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    config.time_zone = 'Tokyo'	
+    config.active_record.default_timezone = :local
   end
 end
