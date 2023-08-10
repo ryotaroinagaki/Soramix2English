cc90091a89128554dc08bcc037249af6781c9af3
github ryotaroinagaki <aaadddryo@gmail.com>
2023-08-10 15:55:21 +0900

    Add: 仮でクイズのページ遷移を設定

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,20 +1,21 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
   end
-
+  
   def show
-
+    @question = Question.find(3)
   end
-
+  
   def difficulty
-
+    @questions = Question.all
   end
 
   def answer
-
+    @question = Question.find(3)
   end
 
   def explanation
-    
+    @question = Question.find(3)
   end
 end
