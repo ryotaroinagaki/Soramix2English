db643efe409dcbe762338db53ef483bf399ff4d2
github ryotaroinagaki <aaadddryo@gmail.com>
2023-08-12 18:12:37 +0900

    Add: 難易度別クイズ一覧に表示で、クイズが5問ずつ表示されるように実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,18 +11,17 @@ class QuestionsController < ApplicationController
     @difficulty = params[:difficulty]
     @questions = Question.where(difficulty: Question.difficulties[@difficulty])
   end
-
+  
   def answer
     @question = Question.find(params[:id])
   end
-
+  
   def explanation
     @question = Question.find(params[:id])
-  end
-
-  def next_question
-    # Logic to get the next question goes here
-    redirect_to question_path(next_question)
+    # i = 5
+    # i -= 1
+    @next_question = Question.where(difficulty: @question.difficulty).where("id > ?", @question.id).first
+    # if 
   end
 
 end
