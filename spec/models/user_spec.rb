83c164ddb7393ae7f384b56c8e92ce2450c2e684
github ryotaroinagaki <aaadddryo@gmail.com>
2023-08-14 12:30:00 +0900

    Add: rails g sorcery:install reset_password --only-submodules実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,19 +2,24 @@
 #
 # Table name: users
 #
-#  id               :bigint           not null, primary key
-#  avatar           :string
-#  crypted_password :string
-#  email            :string           not null
-#  first_name       :string           not null
-#  last_name        :string           not null
-#  salt             :string
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
+#  id                                  :bigint           not null, primary key
+#  access_count_to_reset_password_page :integer          default(0)
+#  avatar                              :string
+#  crypted_password                    :string
+#  email                               :string           not null
+#  first_name                          :string           not null
+#  last_name                           :string           not null
+#  reset_password_email_sent_at        :datetime
+#  reset_password_token                :string
+#  reset_password_token_expires_at     :datetime
+#  salt                                :string
+#  created_at                          :datetime         not null
+#  updated_at                          :datetime         not null
 #
 # Indexes
 #
-#  index_users_on_email  (email) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token)
 #
 require 'rails_helper'
 
