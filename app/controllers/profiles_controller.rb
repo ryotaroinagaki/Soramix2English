d7a949d02addf80a7c7406a0ed047915273586ca
github ryotaroinagaki <aaadddryo@gmail.com>
2023-08-30 17:48:19 +0900

    rails g controller profiles

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,18 @@
+class ProfilesController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
     end
   end
   resources :results, only: %i[create]
+  resource :profiles, only: %i[show edit update]
   get 'on_boardings', to: 'on_boardings#edit'
   resource :on_boardings, only: %i[update]
 end
