ac1da1d09173813ab2290aef0fca4cb3dfb2e514
github ryotaroinagaki <aaadddryo@gmail.com>
2023-09-05 13:11:05 +0900

    Add: Twitter共有機能を実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: '空耳English',
+      # title: '空耳English - 空耳からの脱却',
+      reverse: true,
+      charset: 'utf-8',
+      description: '空耳Englishではクイズに答えて英語の音を学んでいきます',
+      keywords: '空耳,英語,English,空耳英語',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@mslryotaro',
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
