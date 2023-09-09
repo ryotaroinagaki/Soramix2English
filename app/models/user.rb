263e0f917d67a7c4bad4e1f7aeed3ba95790ec37
github ryotaroinagaki <aaadddryo@gmail.com>
2023-09-09 16:45:03 +0900

    Fix: レコメンドメソッド修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,16 +71,25 @@ class User < ApplicationRecord
   #   result.create(question: choice.question, result: false)
   # end
 
-  def similar_users
-    @bookmark_ids = current_user.bookmarks_questions.pluck(:question_id)
-    user_ids = Bookmark.where(question_id: @bookmark_ids).pluck(:user_id)
-    User.where(id: user_ids)
+  def find_similar_users
+    bookmarked_question_ids = current_user.bookmarks_questions.pluck(:question_id)
+    similar_user_ids = Bookmark.where(question_id: bookmarked_question_ids)
+                              .where.not(user_id: current_user.id)
+                              .distinct.pluck(:user_id)
+    User.where(id: similar_user_ids)
   end
 
   def recommend_questions
-    questions_ids = Bookmark.where(user_id: similar_users.ids).distinct.pluck(:question_id)
-    recommend_questions_ids = Question.where(id:questions_ids).where.not(id: @bookmark_ids)
-    recommend_questions = recommend_questions_ids.pluck(:id) - Result.where(user_id: current_user).distinct.pluck(:question_id)
-    recommend_5questions = recommend_questions.sample(3)
+    bookmarked_question_ids = current_user.bookmarks_questions.pluck(:question_id)
+    similar_users = find_similar_users
+    similar_user_question_ids = Bookmark.where(user_id: similar_users.ids)
+                                       .where.not(question_id: bookmarked_question_ids)
+                                       .distinct.pluck(:question_id)
+    recommended_question_ids = Question.where(id: similar_user_question_ids)
+                                       .where.not(id: bookmarked_question_ids)
+                                       .where.not(id: Result.where(user_id: current_user)
+                                                          .distinct.pluck(:question_id))
+                                       .pluck(:id)
+    recommended_questions = Question.where(id: recommended_question_ids).sample(3)
   end
 end
