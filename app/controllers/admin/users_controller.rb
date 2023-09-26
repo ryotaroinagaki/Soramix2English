0f862cafa60d415aa2749dde9f386854d88ef353
github ryotaroinagaki <aaadddryo@gmail.com>
2023-09-26 22:47:18 +0900

    Add: ユーザーの管理画面を作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,14 @@
+class Admin::UsersController < Admin::BaseController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def destory
+    @user.destroy!
+    redirect_to admin_users_path, success: t('defaults.message.deleted', item: User.model_name.human)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -35,5 +35,7 @@ Rails.application.routes.draw do
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
+
+    resources :users, only: %i[index show destory]
   end
 end
