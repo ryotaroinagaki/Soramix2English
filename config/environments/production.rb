659f95e7aedd1acb2cdc6c285a24d482ee51c31e
github ryotaroinagaki <aaadddryo@gmail.com>
2023-10-05 15:19:56 +0900

    Fix: 本番環境でメール送信できるように修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'www.soramix2english.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['GMAIL_ADDRESS'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       :plain,
+    enable_starttls_auto: true,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -91,17 +102,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: Settings.default_url_options[:host] }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default charset: 'utf-8'
-  config.action_mailer.smtp_settings = {
-    enable_starttls_auto: true,
-    address: 'smtp.gmail.com',
-    domain: 'gmail.com',
-    port: 587,
-    user_name: ENV.fetch('GOOGLE_MAIL_ADDRESS', nil),
-    password: ENV.fetch('GOOGLE_APP_PASSWORD', nil),
-    authentication: :login
-  }
+  # config.action_mailer.default_url_options = { host: Settings.default_url_options[:host] }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.default charset: 'utf-8'
+  # config.action_mailer.smtp_settings = {
+  #   enable_starttls_auto: true,
+  #   address: 'smtp.gmail.com',
+  #   domain: 'gmail.com',
+  #   port: 587,
+  #   user_name: ENV.fetch('GOOGLE_MAIL_ADDRESS', nil),
+  #   password: ENV.fetch('GOOGLE_APP_PASSWORD', nil),
+  #   authentication: :login
+  # }
 end
